52d4deadb7a938afd85fe526263bf7d99c4d7827
toddnestor <todd.nestor@gmail.com>
2014-05-05 01:22:07 +0800

    updated routes and links to those routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  devise_scope :user do 
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
   resources :statuses
+
+  get 'feed', to: 'statuses#index', as: :feed
+  get 'feed/create', to: 'statuses#new', as: :feed_create
+
   root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
