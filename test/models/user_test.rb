b44bdee031a3a5b04a44778a4a399aa0a44086f0
toddnestor <todd.nestor@gmail.com>
2014-05-04 23:02:44 +0800

    Wrote validation tests for user registration

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,38 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+test "a user should enter a first name" do
+
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+    end
+
+test "a user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+    end
+
+test "a user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    end
+
+test "a user should have a unique profile name" do
+    user = User.new
+    user.profile_name = users(:todd).profile_name
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    end
+
+test "a user's profile name should have no spaces" do
+    user = User.new
+    user.profile_name = "spaces are in here"
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must not contain any special characters besides underscore or dash.")
+    end
+
 end
