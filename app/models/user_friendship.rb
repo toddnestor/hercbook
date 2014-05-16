7b9287ce757e7178de595f3118aed091f0d14c14
toddnestor <todd.nestor@gmail.com>
2014-05-16 15:03:18 +0800

    users have friendship control, adding and deleting

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -2,6 +2,8 @@ class UserFriendship < ActiveRecord::Base
 
     belongs_to :user
     belongs_to :friend, class_name: "User", foreign_key: "friend_id"
+    
+    after_destroy :delete_mutual_friendship!
 
     state_machine :state, initial: :pending do
         after_transition on: :accept, do: [:send_acceptance_email, :accept_mutual_friendship!]
@@ -38,5 +40,9 @@ class UserFriendship < ActiveRecord::Base
     def accept_mutual_friendship!
         # Grab the mutual friendship and update the state without using the state machine so that no infinite loop was created
         mutual_friendship.update_attribute(:state, 'accepted')
+    end
+
+    def delete_mutual_friendship!
+        mutual_friendship.delete
     end
 end
--- test/models/user_friendship_test.rb
+++ test/models/user_friendship_test.rb
@@ -106,4 +106,30 @@ class UserFriendshipTest < ActiveSupport::TestCase
             end
         end
     end
+
+    context "#delete_mutual_friendship" do
+        setup do
+            UserFriendship.request users(:todd), users(:jason)
+            @friendship1 = users(:todd).user_friendships.where(friend_id: users(:jason).id).first
+            @friendship2 = users(:jason).user_friendships.where(friend_id: users(:todd).id).first
+        end
+        should "delete the mutual friendship" do
+            assert_equal @friendship2, @friendship1.mutual_friendship
+            @friendship1.delete_mutual_friendship!
+            assert !UserFriendship.exists?(@friendship2.id)
+        end
+    end
+
+    context "on destroy" do
+        setup do
+            UserFriendship.request users(:todd), users(:jason)
+            @friendship1 = users(:todd).user_friendships.where(friend_id: users(:jason).id).first
+            @friendship2 = users(:jason).user_friendships.where(friend_id: users(:todd).id).first
+        end
+
+        should "delete the mutual friendship" do
+            @friendship1.destroy
+            assert !UserFriendship.exists?(@friendship2.id)
+        end
+    end
 end
