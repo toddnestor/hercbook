36a800506d469a7d1edd7bc90e5eef84449d4cc6
Todd Nestor <todd.nestor@gmail.com>
2014-06-05 14:41:01 +0800

    updated views for album activities and navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,5 +61,16 @@ module ApplicationHelper
     def devise_mapping
         @devise_mapping ||= Devise.mappings[:user]
     end
-
+    
+    def can_edit_album?(album)
+      signed_in? && current_user == album.user
+    end
+  
+    def album_thumbnail(album)
+        if album.pictures.count > 0
+          image_tag(album.pictures.first.asset.url(:small))
+        else
+          "No Pictures"
+        end
+    end
 end
