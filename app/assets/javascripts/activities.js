f89980f1025a571e5095493aae0ae908d97085fe
Todd Nestor <todd.nestor@gmail.com>
2014-06-14 03:45:55 +0800

    updated album view
    also added autoresize jquery for status entry textarea
    on the activity feed

---
+++ app/assets/javascripts/activities.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('textarea#activityFeedStatus').keypress(function (e) {  
+	       if(e.which == 13)   {   
+	       var control = e.target;                     
+	       var controlHeight = $(control).height();          
+	      //add some height to existing height of control, I chose 17 as my line-height was 17 for the control    
+	    $(control).height(controlHeight+17);  
+	    }
+	    }); 
+
+	$('textarea#activityFeedStatus').blur(function (e) {         
+	    var textLines = $(this).val().trim().split(/\r*\n/).length;  
+	    $(this).val($(this).val().trim()).height(textLines*17);
+	    });
+});
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,7 @@ class Picture < ActiveRecord::Base
   belongs_to :album
   belongs_to :user
   has_attached_file :asset, styles: {
-        large: "800x800>", medium: "300x200>", small: "260x180>", thumb: "80x80#"
+        large: "800x800>", medlg: "500x600>", medium: "300x200>", small: "260x180>", mos: "200x135#", thumb: "80x80#"
     }
   
   def to_s
